e3373fbc797275e0ee9b20b2dfb22ed4ccd1836e
vaseemGit7 <105648020+vaseemGit7@users.noreply.github.com>
2024-11-15 20:23:00 +0530

    Add products route

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 import express from "express";
-import bodyParser from "bodyParser";
+import bodyParser from "body-parser";
+import productRouter from "./routes/products.js";
 
 const app = express();
-const PORT = 3000;
+const PORT = 5000;
 
 app.use(bodyParser.json());
 
 app.listen(PORT, () =>
   console.log(`Server running on the port: http://localhost:${PORT}`)
 );
+
+app.get("/", (req, res) => {
+  res.send("Hello from homepage");
+});
+
+app.use("/products", productRouter);
