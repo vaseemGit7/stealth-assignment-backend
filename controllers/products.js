38d3ecdd6769eb37c2176f26640099f5625572c6
vaseemGit7 <105648020+vaseemGit7@users.noreply.github.com>
2024-11-15 22:31:42 +0530

    Add pagination functionality

--- controllers/products.js
+++ controllers/products.js
@@ -1,7 +1,21 @@
 import products from "../database/productsDB.js";
 
+const paginateProducts = (filteredProducts, pageNumber, pageSize) => {
+  const startIndex = (pageNumber - 1) * pageSize;
+  return filteredProducts.slice(startIndex, startIndex + pageSize);
+};
+
 const getProducts = (req, res) => {
-  res.send(products);
+  const { pageNumber, pageSize } = req.query;
+  const filteredProducts = products;
+
+  const updatedProducts = paginateProducts(
+    filteredProducts,
+    Number(pageNumber),
+    Number(pageSize)
+  );
+
+  res.send(updatedProducts);
 };
 
 export default getProducts;
