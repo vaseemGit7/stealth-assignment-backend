65ff276efd667ec2d5133089c85ce2ba2049aa28
vaseemGit7 <105648020+vaseemGit7@users.noreply.github.com>
2024-11-15 19:53:57 +0530

    Create pesudo database

---
+++ database/productsDB.js
@@ -0,0 +1,54 @@
+const products = [
+  {
+    name: "Classic Denim Jacket",
+    code: 1298743,
+    price: 89.99,
+  },
+  {
+    name: "Silk Wrap Dress",
+    code: 8374612,
+    price: 120.5,
+  },
+  {
+    name: "Leather Chelsea Boots",
+    code: 4567892,
+    price: 145.0,
+  },
+  {
+    name: "Vintage Aviator Sunglasses",
+    code: 9873451,
+    price: 75.25,
+  },
+  {
+    name: "Formal Suit",
+    code: 2354897,
+    price: 250.0,
+  },
+  {
+    name: "High-Waisted Palazzo Pants",
+    code: 6743981,
+    price: 68.4,
+  },
+  {
+    name: "Leather Belt",
+    code: 5632178,
+    price: 35.0,
+  },
+  {
+    name: "Cashmere Scarf",
+    code: 7124563,
+    price: 45.0,
+  },
+  {
+    name: "Slim-Fit Chinos",
+    code: 9845761,
+    price: 54.99,
+  },
+  {
+    name: "Running Shoes",
+    code: 3218945,
+    price: 110.5,
+  },
+];
+
+export default products;
