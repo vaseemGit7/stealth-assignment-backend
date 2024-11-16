9053007919288c763fb9df3cf21a8e4c7118ffcc
vaseemGit7 <105648020+vaseemGit7@users.noreply.github.com>
2024-11-16 10:01:13 +0530

    Add category filter functionality

--- controllers/products.js
+++ controllers/products.js
@@ -1,16 +1,30 @@
 import products from "../database/productsDB.js";
 
+const filterCategories = (filteredProducts, categories) => {
+  if (Array.isArray(categories)) {
+    return filteredProducts.filter((product) =>
+      categories.includes(product["category"])
+    );
+  } else {
+    return filteredProducts.filter(
+      (product) => product["category"] == categories
+    );
+  }
+};
+
 const paginateProducts = (filteredProducts, pageNumber, pageSize) => {
   const startIndex = (pageNumber - 1) * pageSize;
   return filteredProducts.slice(startIndex, startIndex + pageSize);
 };
 
 const getProducts = (req, res) => {
-  const { pageNumber, pageSize } = req.query;
+  const { categories, pageNumber, pageSize } = req.query;
   const filteredProducts = products;
 
+  const categoryFiltered = filterCategories(filteredProducts, categories);
+
   const updatedProducts = paginateProducts(
-    filteredProducts,
+    categoryFiltered,
     Number(pageNumber),
     Number(pageSize)
   );
