801083ef7243f15e95f819b52db14ed4267cefd2
vaseemGit7 <105648020+vaseemGit7@users.noreply.github.com>
2024-11-18 15:44:58 +0530

    Update code with cors

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 import express from "express";
 import bodyParser from "body-parser";
 import productRouter from "./routes/products.js";
+import cors from "cors"
 
 const app = express();
 const PORT = 5000;
 
+app.use(cors())
 app.use(bodyParser.json());
 
 app.listen(PORT, () =>
